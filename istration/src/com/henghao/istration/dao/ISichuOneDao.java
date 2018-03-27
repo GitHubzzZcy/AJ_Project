dbc52ddd97f5724f4a43071d9c2d23796a450529
zcy <824318340@qq.com>
2018-03-27 17:28:21 +0800

    第二次提交Git

--- istration/src/com/henghao/istration/dao/ISichuOneDao.java
+++
@@ -1,8 +0,0 @@
-package com.henghao.istration.dao;
-
-import com.henghao.istration.entity.SichuOne;
-import com.henghao.news.dao.IBaseDao;
-
-public interface ISichuOneDao extends IBaseDao<SichuOne> {
-
-}
